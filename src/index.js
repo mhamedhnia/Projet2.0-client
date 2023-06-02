1cb352d4d9e55f965f117fd73a70c71ad4facbb7
Mhamed Hnia <hniamhamed@gmail.com>
2023-06-02 01:06:25 +0100

    upgraded to react18 and migrated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -7,13 +7,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './JS/store/store';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
